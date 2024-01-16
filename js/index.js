407e0a152d12d33c40621eac7aa07837c588afa2
GustavoGonzales <gustavo.gonzales.tec@gmail.com>
2024-01-16 11:53:09 -0300

    Desafio Finalizado

--- js/index.js
+++ js/index.js
@@ -9,11 +9,7 @@ btnSubmit.addEventListener("click", () => {
     const cidade = document.querySelector('input[name="inputCidade"]').value;
     const email = document.querySelector('input[name="inputEmail"]').value;
     const complemento = document.querySelector('input[name="inputComplemento"]').value;
-    
-    
-    const uf = document.getElementById('inputUF');
-    const valueuf = select.options[select.selectedIndex].value;
-
+    const uf = document.getElementById('inputUF').value;
     const telefone = document.querySelector('input[name="inputTelefone"]').value;
     const bairro = document.querySelector('input[name="inputBairro"]').value;
     const cep = document.querySelector('input[name="inputCEP"]').value;
@@ -22,12 +18,12 @@ btnSubmit.addEventListener("click", () => {
     localStorage.setItem("endereco", endereco);
     localStorage.setItem("cidade", cidade);
     localStorage.setItem("email", email);
-    localStorage.setItem("valueuf", valueuf);
+    localStorage.setItem("uf", uf);
     localStorage.setItem("telefone", telefone);
     localStorage.setItem("bairro", bairro);
     localStorage.setItem("cep", cep);
     localStorage.setItem("complemento", complemento);
 
-    //window.location.replace("../ResumoCadastro/Resumo.html");
-    alert(valueuf);
+    window.location.replace("../ResumoCadastro/Resumo.html");
+    
 });
